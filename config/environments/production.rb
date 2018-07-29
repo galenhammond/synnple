62c98c289026e26422fd348e7c26180d8189d71c
Galen Hammond <galenhammond@live.ca>
2018-07-29 19:53:31 -0400

    production fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
     :enable_starttls_auto => true
-   # }
+    }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
